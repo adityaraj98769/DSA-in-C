2f7182169c502008ccc95067c7f1d2e2b7bcff6e
adityaraj98769 <166215470+adityaraj98769@users.noreply.github.com>
2024-08-28 12:06:03 +0530

    Add files via upload

---
+++ structure.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<string.h>
+struct Student {
+       char name[50];
+       float marks;
+       int roll_no;
+
+    }s1;
+    
+int main(){
+    
+    printf("Enter the Student name : ");
+    scanf("%s",&s1.name);
+    printf("Enter the marks : ");
+    scanf("%f",&s1.marks);
+    printf("Enter the Roll number : ");
+    scanf("%d",&s1.roll_no);
+
+    printf("Student Details are given below: \n");
+    printf("NAME = %s \n",s1.name);
+    printf("MARKS = %f \n", s1.marks);
+    printf("ROLL NO = %d \n", s1.roll_no);
+
+    return 0;
+}
